59bdeaf11209526fc03c267493507d8d75b1a5a1
chiyaki <66chiyaki@gmail.com>
2016-12-26 05:05:00 +0800

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/apps/job-listing"
 set :linked_files, fetch(:linked_files, []).push("config/database.yml", "config/secrets.yml")
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle"
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle")
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
